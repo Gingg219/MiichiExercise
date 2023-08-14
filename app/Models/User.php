987d2c50e43ec02d6ad41435a522f4ed9d55a8d7
Unta01 <76728492+Gingg219@users.noreply.github.com>
2023-08-15 02:48:32 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Observers\UserObserver;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthAuthenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
